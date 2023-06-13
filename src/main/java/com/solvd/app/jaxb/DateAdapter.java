473c00a040a350c26a13606c58dfbf1998549528
hhan-solvd <130609200+hhan-solvd@users.noreply.github.com>
2023-06-13 08:01:36 -0500

    Update src/main/java/com/solvd/app/jaxb/DateAdapter.java
    Co-authored-by: ankochergin <103670717+ankochergin@users.noreply.github.com>

--- src/main/java/com/solvd/app/jaxb/DateAdapter.java
+++ src/main/java/com/solvd/app/jaxb/DateAdapter.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class DateAdapter extends XmlAdapter<String, Date> {
+
     @Override
     public Date unmarshal(String s) throws Exception {
         return new Date(new SimpleDateFormat("yyyy-MM-dd").parse(s).getTime());
